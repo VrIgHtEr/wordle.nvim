c9ba51ce554d1e1e556fa1eb76cdab6c0ee04997
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-16 17:00:27 +0100

    started ui

---
+++ lua/wordle/ui.lua
@@ -0,0 +1,25 @@
+local ui = {}
+local Popup = require 'nui.popup'
+local event = require('nui.utils.autocmd').event
+
+local popup = Popup {
+    enter = true,
+    focusable = true,
+    border = { style = 'rounded' },
+    position = '50%',
+    size = { width = '80%', height = '60%' },
+    buf_options = {
+        modifiable = true,
+        readonly = false,
+    },
+}
+popup:mount()
+popup:on(event.TextChangedI, function(...)
+    print(vim.inspect { ... })
+end)
+
+popup:on(event.BufLeave, function()
+    popup:unmount()
+end)
+
+return ui
